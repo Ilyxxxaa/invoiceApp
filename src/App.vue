a9642c73d9632b33e3b3efd59be1b6cde602bd5a
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-04-24 23:15:47 +0300

    feat: add navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <template>
-  <router-view />
+  <div>
+    <div v-if="!mobile" class="app flex">
+      <navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or Tablet</p>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import Navigation from "./components/Navigation.vue";
+export default {
+  data() {
+    return {
+      mobile: false,
+    };
+  },
+  components: {
+    Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,7 +49,34 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+
+.app {
   background-color: #141625;
+  min-height: 100vh;
+  flex-direction: column;
+  @media (min-width: 900px) {
+    flex-direction: row;
+  }
+
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #ffffff;
+
+  p {
+    margin-top: 16px;
+  }
 }
 
 button,
---
+++ src/components/InvoiceModal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    @click="checkClick"
+    ref="invoiceWrap"
+    class="invoice-wrap flex flex-column"
+  >
+    <form @submit.prevent="submitForm" class="invoice-content">
+      <h1>New Invoice</h1>
+      <div class="bill-from flex flex-column">
+        <h4>Bill From</h4>
+        <div class="input flex flex-column">
+          <label for="billerStreetAddress">Street Address</label>
+          <input required type="text" id="billerStreetAddress" />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "invoiceModal",
+  data() {
+    return {
+      docId: null,
+      loading: null,
+      billerStreetAddress: null,
+      billerCity: null,
+      billerZipCode: null,
+      billerCountry: null,
+      clientName: null,
+      clientEmail: null,
+      clientStreetAddress: null,
+      clientCity: null,
+      clientZipCode: null,
+      clientCountry: null,
+      invoiceDateUnix: null,
+      invoiceDate: null,
+      paymentTerms: null,
+      paymentDueDateUnix: null,
+      paymentDueDate: null,
+      productDescription: null,
+      invoicePending: null,
+      invoiceDraft: null,
+      invoiceItemList: [],
+      invoiceTotal: 0,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
